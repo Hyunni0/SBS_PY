fae278c061c1abbbaefa7c6ed1fe68bffd808427
Hyunni0 <hyunn09153@gmail.com>
2022-01-27 15:48:28 +0900

    9일차

---
+++ Day10/ex03.py
@@ -0,0 +1,20 @@
+import time
+
+# 파일 저장 경로
+path = 'C:/DH/SBS_PY/SBS_PY/Day10/'
+
+today = time.strftime('%Y - %m - %d')
+
+# 파일 열기 : open(파일명, 모드, 옵션)
+file = open(path + '할일(' + today +')' + '.txt', 'at', encoding = 'UTF-8')
+
+no = 1
+while True :
+    todo = input('오늘 할 일 : ')
+    # 아무 값도 입력하지 않은 경우 - 종료
+    if not todo :
+        break
+    file.write(str(no) + '. ' + todo + '\n')
+    no = no + 1
+    
+file.close
